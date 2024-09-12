d823077a2e0d3a3b9b142af1ccaa8bfe2f40ecb4
Enriquito004 <enriquenodarse55@gmail.com>
2024-09-11 22:14:15 -0400

    PruebasTerminadas

--- test/testFactura/PruebaFactura.java
+++ test/testFactura/PruebaFactura.java
@@ -26,28 +26,6 @@ public class PruebaFactura {
 
     }
 
-    @Test
-    public void testUpdateFactura() {
-        //VARS
-        double montoAntiguo = 0;
-        double montoActualizado = 0;
-
-        //INSTANCES
-        Etecsa etc = new Etecsa();
-        Date fechaEmision = new Date();
-        Facturas factura2 = new Facturas("1010", "203", 20.56, fechaEmision);
-        etc.addFactura(factura2);
-
-        /*TOMAR VALOR DEL MONTO DE LA FACTURA EN LA LISTA*/
-        montoAntiguo = etc.lista.get(0).getMontoFactura();
-
-        /*UPDATE MONTO DE LA FACTURA Y RETOMAR VALOR DE FACTURA*/
-        etc.lista.get(0).setMontoFactura(38.99);
-        montoActualizado = etc.lista.get(0).getMontoFactura();
-
-        /*ES FALSO QUE SON IGUALES LOS VALORES ANTES
-         Y DESPUES DE ACTUALIZAR*/
-        org.junit.Assert.assertFalse(montoAntiguo == montoActualizado);
-    }
+    
 
 }
